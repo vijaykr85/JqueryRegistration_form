253e246ee604761bda5d909882168782b592f46b
Vijay kumar <96686531+vijaykr85@users.noreply.github.com>
2024-02-16 14:11:12 +0530

    Add project files.

---
+++ JqueryRegistration_form/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JqueryRegistration_form.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
